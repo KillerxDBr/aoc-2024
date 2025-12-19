8c7f4022a2b37124622c7f3648a5f50ded1c498b
KxD-Work <antonioroberto1407@gmail.com>
2025-12-19 12:31:30 -0400

    Start

---
+++ src/day01.rs
@@ -0,0 +1,81 @@
+use std::cmp::max;
+use std::iter;
+use std::{env, str::FromStr};
+
+fn load_data() -> Result<String, std::io::Error> {
+    let args: Vec<String> = env::args().collect();
+
+    let path: String;
+    if args.len() > 1 {
+        path = args[1].clone();
+    } else {
+        path = String::from_str("input/day01/input.txt").unwrap();
+    }
+
+    return std::fs::read_to_string(path);
+}
+
+fn part1(lhs: &Vec<u64>, rhs: &Vec<u64>) -> u64 {
+    let mut lhs_sorted: Vec<u64> = lhs.clone();
+    let mut rhs_sorted: Vec<u64> = rhs.clone();
+
+    lhs_sorted.sort();
+    rhs_sorted.sort();
+
+    assert_eq!(lhs_sorted.len(), rhs_sorted.len());
+
+    let mut result = 0;
+
+    for i in 0..lhs_sorted.len() {
+        let diff = lhs_sorted[i].abs_diff(rhs_sorted[i]);
+        // println!("{}, {} = {}", &lhs[i], &rhs[i], diff);
+        result += diff;
+    }
+
+    return result;
+}
+
+fn part2(lhs: &Vec<u64>, rhs: &Vec<u64>) -> u64 {
+    let mut result: u64 = 0;
+
+    // let r_max= ;
+    // let l_max= ;
+    let vec_max = *max(rhs.iter().max().unwrap(), lhs.iter().max().unwrap()) as usize + 1;
+    // let mut rst: Vec<u8> = Vec::with_capacity(vec_max as usize);
+    // println!("Reserving Vector with {} slots", vec_max);
+    let mut rst: Vec<u64> = iter::repeat(0).take(vec_max).collect();
+
+    for v in rhs {
+        rst[*v as usize] += 1;
+    }
+
+    for i in lhs {
+        if rst[*i as usize] == 0 {
+            continue;
+        }
+        // println!("{} += {} * {}", result, i, rst[*i as usize]);
+        result += *i * rst[*i as usize];
+    }
+
+    return result;
+}
+
+fn main() {
+    println!("aeHOOOOO");
+    let content = load_data().unwrap();
+
+    let mut lhs: Vec<u64> = Vec::new();
+    let mut rhs: Vec<u64> = Vec::new();
+
+    for line in content.trim().split('\n') {
+        // println!("Line: {}", line);
+        let mut c = line.split_ascii_whitespace();
+        lhs.push(c.next().unwrap().parse().unwrap());
+        rhs.push(c.next().unwrap().parse().unwrap());
+
+        assert_eq!(None, c.next());
+    }
+
+    println!("Resultado Parte 1: {}", part1(&lhs, &rhs));
+    println!("Resultado Parte 2: {}", part2(&lhs, &rhs));
+}
