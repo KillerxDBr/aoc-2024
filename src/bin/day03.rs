15ff0f711cba6775e179e4ab5cc9c53e981d13ea
KillerxDBr <97485370+KillerxDBr@users.noreply.github.com>
2026-01-03 16:19:13 -0400

    day03 pt2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -15,7 +15,60 @@ fn part1(content: &String) -> u64 {
     return result;
 }
 
+fn part2(content: &String) -> u64 {
+    let mut result = 0;
+
+    // type DoRange = (usize, usize, bool);
+    // let mut ranges: Vec<DoRange> = Vec::new();
+
+    let mut ok = true;
+    let mut i: usize = 0;
+    let sz = content.len();
+    while i < sz {
+        let do_loc: usize;
+        let dont_loc: usize;
+
+        // println!("==============================");
+        // println!("i:    {i}");
+        if let Some(x) = content[i..].find(r"do()") {
+            // i += x;
+            do_loc = x;
+        } else {
+            do_loc = usize::MAX;
+        }
+        if let Some(x) = content[i..].find(r"don't()") {
+            // i += x;
+            dont_loc = x;
+        } else {
+            dont_loc = usize::MAX;
+        }
+
+        let str_end: usize;
+        if ok {
+            if dont_loc != usize::MAX {
+                str_end = (i + dont_loc + 1).min(sz - 1);
+            } else {
+                str_end = sz;
+            }
+            let str = content[i..str_end].to_string();
+            result += part1(&str);
+            i = str_end;
+            ok = false;
+        } else {
+            if do_loc != usize::MAX {
+                str_end = (i + do_loc + 1).min(sz - 1);
+            } else {
+                str_end = sz;
+            }
+            i = str_end;
+            ok = true
+        }
+    }
+    return result;
+}
+
 fn main() {
     let content = utils::load_data().unwrap();
     println!("Resultado Parte 1: {}", part1(&content));
+    println!("Resultado Parte 2: {}", part2(&content));
 }
